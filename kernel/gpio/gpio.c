cddacfe492470c5898695fec332d1722f6823ac0
cronyx <xcronyx@gmail.com>
2023-06-04 11:02:18 +0300

    oss3 to t40

--- kernel/gpio/gpio.c
+++ kernel/gpio/gpio.c
@@ -1,11 +1,9 @@
-#include <jz_proc.h>
+//#include <jz_proc.h>
 #include <linux/proc_fs.h>
 #include <linux/gpio.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/types.h>
-
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("OpenIPC");
@@ -14,8 +12,6 @@ MODULE_DESCRIPTION("Ingenic GPIO Claimer");
 static struct proc_dir_entry *claim_proc;
 
 int claim_gpio(int gpio) {
-  int ret = 0;
-
   pr_debug("GPIO[%i] Requesting...\n", gpio);
 
   if (!gpio_is_valid(gpio)) {
@@ -34,11 +30,10 @@ int claim_gpio(int gpio) {
   return 0;
 }
 
-ssize_t claim_proc_write(struct file *filp, const char *buf, size_t len,
-                         loff_t *off) {
+ssize_t claim_proc_write(struct file *filp, const char *buf, size_t len, loff_t *off) {
+  int gpio;
   int ret = 0;
   char cmd[4] = {0};
-  uint32_t addr, data, datalen;
 
   if (len > 4) {
     return -EFAULT;
@@ -46,7 +41,7 @@ ssize_t claim_proc_write(struct file *filp, const char *buf, size_t len,
   if (copy_from_user(cmd, buf, len)) {
     return -EFAULT;
   }
-  int gpio = simple_strtoul(cmd, NULL, 0);
+  gpio = simple_strtoul(cmd, NULL, 0);
   ret = claim_gpio(gpio);
   if (ret) {
     pr_err("GPIO[%i] Error %i \n", gpio, ret);
