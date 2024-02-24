18b18758de6096ad0a404d422317fb351bb95575
Paul Philippov <themactep@gmail.com>
2024-02-24 05:08:06 -0500

    rewrite script for local compilation

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <soc> <build|clean>"
-  exit 1
-fi
+case "$1" in
+	clean)
+		find . -name "*.o" -type f -exec rm -f {} \;
+		find . -name "*.o.*" -type f -exec rm -f {} \;
+		find . -name "*.ko" -type f -exec rm -f {} \;
+		find . -name "*.ko.*" -type f -exec rm -f {} \;
+		find . -name "*.mod.c" -type f -exec rm -f {} \;
+		rm -f Module.symvers
+		rm -f modules.order
+		rm -rf .tmp_versions
+		;;
 
-export OUTPUT_DIR=/home/paul/openipc-fw-output/cinnado_d1-br2023.11
-export PATH=$OUTPUT_DIR/host/bin:$PATH
-export CROSS_COMPILE=mipsel-openipc-linux-musl-
-export ISP_ENV_KERNEL_DIR=$OUTPUT_DIR/build/linux-custom
-export SOC_FAMILY=$1
+	t10|t20|t21|t23|t30|t31|t40|t41)
+		export SOC_FAMILY=$1
+		export PATH=/opt/toolchains/thingino/mipsel-thingino-linux-musl_sdk-buildroot/bin:$PATH
+		export CROSS_COMPILE=mipsel-linux-
+		export ISP_ENV_KERNEL_DIR=${HOME}/output/escam-g16-t21n-jxh62/build/linux-custom 
+		export OUTPUT_DIR=${HOME}/output/openingenic
 
-if [[ "$2" == "build" ]]; then
-    make ARCH=mips -C $ISP_ENV_KERNEL_DIR M=$PWD
+		[ -d "$OUTPUT_DIR" ] && mkdir -p "$OUTPUT_DIR"
 
-    #ccache make V=2 \
-    #    ARCH=mips CROSS_COMPILE=/path/to/openipc/firmware/output/host/bin/mipsel-openipc-linux-musl- \
-    #    -C /path/to/openipc/firmware/output/build/linux-custom/ \
-    #    SOC=t31 \
-    #    M=$PWD -j8
+		FAM="SOC_FAMILY=${SOC_FAMILY} CONFIG_SOC_${SOC_FAMILY^^}=y"
+		make ARCH=mips $FAM -C $ISP_ENV_KERNEL_DIR M=$PWD ${@:2}
+		#ccache make V=2 \
+		#    ARCH=mips CROSS_COMPILE=/path/to/openipc/firmware/output/host/bin/mipsel-openipc-linux-musl- \
+		#    -C /path/to/openipc/firmware/output/build/linux-custom/ \
+		#    SOC=t31 \
+		#    M=$PWD -j8
+		;;
+	*)
+		echo "Usage: $0 <soc|clean>"
+		;;
+esac
 
-elif [[ "$2" == "clean" ]]; then
-    find . -name "*.o" -type f -exec rm -f {} \;
-    find . -name "*.o.*" -type f -exec rm -f {} \;
-    find . -name "*.ko" -type f -exec rm -f {} \;
-    find . -name "*.ko.*" -type f -exec rm -f {} \;
-    find . -name "*.mod.c" -type f -exec rm -f {} \;
-    rm -f Module.symvers
-    rm -f modules.order
-    rm -rf .tmp_versions
-
-elif [[ "$2" == "" ]]; then
-    echo "build, or clean"
-fi
+exit 0
