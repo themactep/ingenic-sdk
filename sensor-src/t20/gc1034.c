f40c815aa26dcc76a5a4aa40cb94a4896f187727
gtxaspec <gtxent@gmail.com>
2024-03-18 02:22:46 -0700

    t20 add sensor version define

--- sensor-src/t20/gc1034.c
+++ sensor-src/t20/gc1034.c
@@ -38,6 +38,7 @@
 #define SENSOR_SUPPORT_PCLK (39*1000*1000)
 #define SENSOR_OUTPUT_MAX_FPS 30
 #define SENSOR_OUTPUT_MIN_FPS 5
+#define SENSOR_VERSION
 #define DRIVE_CAPABILITY_2
 
 static struct sensor_info sensor_info = {
