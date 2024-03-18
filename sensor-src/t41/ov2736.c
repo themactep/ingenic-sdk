4b16acee19295209d27f55af3b1a3a7022d368f7
Paul Philippov <themactep@gmail.com>
2024-03-18 07:24:54 -0400

    fixes

--- sensor-src/t41/ov2736.c
+++ sensor-src/t41/ov2736.c
@@ -1031,7 +1031,7 @@ static struct tx_isp_subdev_video_ops sensor_video_ops = {
 	.s_stream = sensor_s_stream,
 };
 
-static struct tx_isp_subdev_sensor _ops sensor_sensor_ops = {
+static struct tx_isp_subdev_sensor_ops sensor_sensor_ops = {
 	.ioctl = sensor_sensor_ops_ioctl,
 };
 
