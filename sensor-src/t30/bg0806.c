be5da7e481b7bd257237bf622e9c992aaf511218
Paul Philippov <themactep@gmail.com>
2024-03-18 20:43:35 -0400

    add SENSOR_NAME

--- sensor-src/t30/bg0806.c
+++ sensor-src/t30/bg0806.c
@@ -17,6 +17,7 @@
 #include <tx-isp-common.h>
 #include <sensor-common.h>
 
+#define SENSOR_NAME "bg0806"
 #define SENSOR_CHIP_ID_H (0x08)
 #define SENSOR_CHIP_ID_L (0x06)
 #define SENSOR_REG_END 0xffff
@@ -109,7 +110,7 @@ struct tx_isp_dvp_bus sensor_dvp={
 };
 
 struct tx_isp_sensor_attribute sensor_attr={
-	.name = "bg0806",
+	.name = SENSOR_NAME,
 	.chip_id = 0x0806,
 	.cbus_type = TX_SENSOR_CONTROL_INTERFACE_I2C,
 	.cbus_mask = V4L2_SBUS_MASK_SAMPLE_8BITS | V4L2_SBUS_MASK_ADDR_16BITS,
@@ -765,7 +766,7 @@ static int sensor_g_chip_ident(struct tx_isp_subdev *sd,
 	}
 	printk("%s chip found @ 0x%02x (%s)\n", SENSOR_NAME, client->addr, client->adapter->name);
 	if (chip) {
-		memcpy(chip->name, "bg0806", sizeof("bg0806"));
+		memcpy(chip->name, SENSOR_NAME, sizeof(SENSOR_NAME));
 		chip->ident = ident;
 		chip->revision = SENSOR_VERSION;
 	}
@@ -890,7 +891,7 @@ static struct tx_isp_subdev_ops sensor_ops = {
 /* It's the sensor device */
 static u64 tx_isp_module_dma_mask = ~(u64)0;
 struct platform_device sensor_platform_device = {
-	.name = "bg0806",
+	.name = SENSOR_NAME,
 	.id = -1,
 	.dev = {
 		.dma_mask = &tx_isp_module_dma_mask,
@@ -993,7 +994,7 @@ static int sensor_remove(struct i2c_client *client)
 }
 
 static const struct i2c_device_id sensor_id[] = {
-	{ "bg0806", 0 },
+	{ SENSOR_NAME, 0 },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, sensor_id);
@@ -1001,7 +1002,7 @@ MODULE_DEVICE_TABLE(i2c, sensor_id);
 static struct i2c_driver sensor_driver = {
 	.driver = {
 		.owner = THIS_MODULE,
-		.name = "bg0806",
+		.name = SENSOR_NAME,
 	},
 	.probe = sensor_probe,
 	.remove = sensor_remove,
