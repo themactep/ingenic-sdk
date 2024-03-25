422a11226b11d79c3e1fc0e575b137dfe73f2cdd
Paul Philippov <themactep@gmail.com>
2024-03-25 06:29:01 -0400

    clean up

--- sensor-src/t41zrt/gc4023.c
+++ sensor-src/t41zrt/gc4023.c
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
  * gc4023.c
- *
  * Copyright (C) 2022 Ingenic Semiconductor Co., Ltd.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 
 #include <linux/init.h>
