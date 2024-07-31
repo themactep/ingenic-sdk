40e4da47614a3d914acc2a788998f98eb00f75e1
gtxaspec <gtxent@gmail.com>
2024-07-31 16:57:07 -0700

    gpio-keys: don't use polled, use existing platform data

--- 3.10/misc/gpio-keys/gpio-keys.c
+++ 3.10/misc/gpio-keys/gpio-keys.c
@@ -1,78 +1,140 @@
 #include <linux/module.h>
-#include <linux/kernel.h>
 #include <linux/platform_device.h>
-#include <linux/input.h>
 #include <linux/gpio_keys.h>
+#include <linux/slab.h>
+#include <linux/input.h>
 
-#define MAX_BUTTONS 10
+extern struct platform_device jz_button_device;
+extern struct gpio_keys_button board_buttons[];
+extern struct gpio_keys_platform_data board_button_data;
 
-static struct gpio_keys_button gpio_buttons[MAX_BUTTONS];
-static struct gpio_keys_platform_data gpio_keys_data = {
-    .buttons = gpio_buttons,
-    .nbuttons = 0,
-    .poll_interval = 20, // Polling interval in milliseconds
-};
+#define MAX_ADDITIONAL_BUTTONS 10
 
-static struct platform_device gpio_keys_device = {
-    .name = "gpio-keys-polled", // Using "gpio-keys-polled" for the polled version to co-exist
-    .id = -1,
-    .dev = {
-        .platform_data = &gpio_keys_data,
-    },
-};
+static struct gpio_keys_button *new_buttons;
+static struct gpio_keys_platform_data new_button_data;
+static struct platform_device *new_button_device;
 
 static char *gpio_config = "";
 module_param(gpio_config, charp, 0000);
 MODULE_PARM_DESC(gpio_config, "GPIO configuration: <KEYCODE,GPIO,ACTIVE_LOW>;...");
 
-static int __init parse_gpio_config(void)
+static int parse_gpio_config(void)
 {
-    char *token;
-    char *cur = gpio_config;
-    int button_count = 0;
+	char *token;
+	char *cur;
+	int button_count = 0;
+	int existing_buttons = board_button_data.nbuttons;
 
-    while ((token = strsep(&cur, ";")) != NULL && button_count < MAX_BUTTONS) {
-        int keycode, gpio, active_low;
-        if (sscanf(token, "%d,%d,%d", &keycode, &gpio, &active_low) == 3) {
-            gpio_buttons[button_count].code = keycode;
-            gpio_buttons[button_count].gpio = gpio;
-            gpio_buttons[button_count].active_low = active_low;
-            gpio_buttons[button_count].desc = "GPIO Button";
-            gpio_buttons[button_count].type = EV_KEY;
-            button_count++;
-        }
-    }
-    gpio_keys_data.nbuttons = button_count;
-    return 0;
+	cur = kstrdup(gpio_config, GFP_KERNEL);
+	if (!cur)
+		return -ENOMEM;
+
+	while ((token = strsep(&cur, ";")) != NULL && button_count < MAX_ADDITIONAL_BUTTONS) {
+		int keycode, gpio, active_low;
+		if (sscanf(token, "%d,%d,%d", &keycode, &gpio, &active_low) == 3) {
+			new_buttons[existing_buttons + button_count].code = keycode;
+			new_buttons[existing_buttons + button_count].gpio = gpio;
+			new_buttons[existing_buttons + button_count].active_low = active_low;
+			new_buttons[existing_buttons + button_count].desc = "GPIO Button";
+			new_buttons[existing_buttons + button_count].type = EV_KEY;
+			button_count++;
+		}
+	}
+
+	new_button_data.buttons = new_buttons;
+	new_button_data.nbuttons = existing_buttons + button_count;
+	kfree(cur);
+	return 0;
 }
 
-static int __init gpio_keys_init(void)
+static int __init add_gpio_keys_init(void)
 {
-    int ret;
+	int ret, i, existing_buttons, additional_buttons = 0;
+	char *cur;
 
-    ret = parse_gpio_config();
-    if (ret)
-        return ret;
+	existing_buttons = board_button_data.nbuttons;
 
-    ret = platform_device_register(&gpio_keys_device);
-    if (ret)
-        printk(KERN_ERR "Unable to register gpio-keys-polled device\n");
-    else
-        printk(KERN_INFO "gpio-keys-polled device registered\n");
+	// Count additional buttons specified in gpio_config
+	cur = kstrdup(gpio_config, GFP_KERNEL);
+	if (cur) {
+		while (strsep(&cur, ";") != NULL && additional_buttons < MAX_ADDITIONAL_BUTTONS) {
+			additional_buttons++;
+		}
+		kfree(cur);
+	}
 
-    return ret;
+	// Allocate memory for new buttons array
+	new_buttons = kzalloc(sizeof(struct gpio_keys_button) * (existing_buttons + additional_buttons), GFP_KERNEL);
+	if (!new_buttons) {
+		return -ENOMEM;
+	}
+
+	// Copy existing buttons to new buttons array
+	for (i = 0; i < existing_buttons; i++) {
+		new_buttons[i] = board_buttons[i];
+	}
+
+	// Parse and add new buttons
+	ret = parse_gpio_config();
+	if (ret) {
+		kfree(new_buttons);
+		return ret;
+	}
+
+	// Initialize new platform data
+	new_button_data.buttons = new_buttons;
+	new_button_data.nbuttons = existing_buttons + additional_buttons;
+
+	// Allocate and register the new platform device
+	new_button_device = platform_device_alloc("gpio-keys", -1);
+	if (!new_button_device) {
+		ret = -ENOMEM;
+		goto err_alloc;
+	}
+
+	new_button_device->dev.platform_data = &new_button_data;
+
+	// Unregister the existing device
+	platform_device_unregister(&jz_button_device);
+
+	// Register the new device
+	ret = platform_device_add(new_button_device);
+	if (ret) {
+		platform_device_put(new_button_device);
+		goto err_alloc;
+	}
+
+	printk(KERN_INFO "Additional GPIO keys device registered with %d buttons\n", new_button_data.nbuttons);
+	return 0;
+
+err_alloc:
+	kfree(new_buttons);
+	return ret;
 }
 
-static void __exit gpio_keys_exit(void)
+static void __exit add_gpio_keys_exit(void)
 {
-    platform_device_unregister(&gpio_keys_device);
-    printk(KERN_INFO "gpio-keys-polled device unregistered\n");
+	// Clear the additional buttons data
+	int existing_buttons = board_button_data.nbuttons;
+	int i;
+
+	for (i = existing_buttons; i < new_button_data.nbuttons; i++) {
+		new_buttons[i].code = 0;
+		new_buttons[i].gpio = 0;
+		new_buttons[i].active_low = 0;
+		new_buttons[i].desc = NULL;
+		new_buttons[i].type = 0;
+	}
+
+	printk(KERN_INFO "Cleared additional GPIO keys data\n");
+
+	kfree(new_buttons);
 }
 
-module_init(gpio_keys_init);
-module_exit(gpio_keys_exit);
+module_init(add_gpio_keys_init);
+module_exit(add_gpio_keys_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("gtxaspec/thingino");
-MODULE_DESCRIPTION("A GPIO Keys Polled Kernel Module with Configurable GPIOs");
-MODULE_VERSION("0.5");
+MODULE_DESCRIPTION("Add gpio-keys Dynamically");
+MODULE_VERSION("0.1");
